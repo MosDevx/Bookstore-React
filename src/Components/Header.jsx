1b4b73f218bbe9f1818fbd9b114b1013828970e2
MosDevx <mosesk.wamae@gmail.com>
2022-12-24 20:36:47 +0300

    fix header styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,8 @@ const Header =()=>{
 				<div className="flex justify-between container m-auto items-center h-24 bg-white border-b-2 p-2">
 						<div className="w-1/2 flex justify-evenly text-2xl items-center">
 								<h2 className="text-5xl font-bold text-blue-500">Bookstore CMS</h2>
-								<Link className='text-slate-500 active:text-black' to={'/home'} >Books</Link>
-								<Link className='text-slate-500 active:text-black' to={'/categories'}>Categories</Link>
+								<Link className='text-slate-500 hover:text-black' to={'/home'} >Books</Link>
+								<Link className='text-slate-500 hover:text-black' to={'/categories'}>Categories</Link>
 						</div>
 						
 						<div>
