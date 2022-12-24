0d9423675e12085c959f549a8ecb7722eb9a731d
MosDevx <mosesk.wamae@gmail.com>
2022-12-24 19:47:57 +0300

    style Book Component

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -13,36 +13,36 @@ const Book = ({title,author,item_id,category})=>{
 	return(
 		<>
 		{/* main-div */}
-		<div className=" flex border-2 h-32 m-2 items-center">
+		<div className=" flex border-2 h-36 m-2 items-center">
 
 				{/*First half - Details and complete Icon */}
 			<div className='w-3/4 flex justify-between'>
 
 					{/* Details */}
 					<div className='flex flex-col pl-10 text-left'>
-							<span>{category}</span>
-							<span className='capitalize font-bold'>{title}</span>
-							<span className='capitalize'>{author}</span>
+							<span className="font-bold  text-slate-600">{category}</span>
+							<span className='capitalize font-bold text-2xl'>{title}</span>
+							<span className='capitalize text-blue-500 text-lg'>{author}</span>
 
-							<div className='space-x-4'>
-								<button className='border p-1 font-bold'>Comments</button>
-								<button className='border p-1 font-bold' onClick={()=>onRemoveButtonClicked(item_id)}>Remove</button>
-								<button className='border p-1 font-bold' >Edit</button>
+							<div className='space-x-4 mt-4 text-blue-500'>
+								<button className='border-r-2 p-1 pr-2 font-bold'>Comments</button>
+								<button className='border-r-2 p-1 pr-2 font-bold' onClick={()=>onRemoveButtonClicked(item_id)}>Remove</button>
+								<button className=' p-1 font-bold' >Edit</button>
 							</div>
 					</div>
 
 						{/*Complete Icon  */}
 					<div className='flex justify-between  items-center border-r-4 pr-16 border-slate-400'>
 						<CircularProgress variant="determinate" size={80} thickness={4} value={70} />
-						<span className='pl-8'>70% <br />Completed</span>
+						<span className='pl-8 text-slate-400'> <span className='text-4xl text-black'>70%</span>  <br />Completed</span>
 					</div>
 			</div>
 
 				{/*Second half- Current Chapter  */}
-			<div className='w-1/4 flex flex-col h-28   pl-16 justify-evenly'>
-				<span>Current Chapter</span>
-				<span>Chapter 17</span>
-				<button className='w-fit border-2 p-2 '>Update Progress</button>
+			<div className='w-1/4 flex flex-col h-28  pl-16 justify-evenly'>
+				<span className='text-slate-400 uppercase'>Current Chapter</span>
+				<span className='capitalize mb-4'>Chapter 17</span>
+				<button className='w-fit border-2 p-2 uppercase bg-blue-500 text-white'>Update Progress</button>
 			</div>
 		</div>
 		</>
